e0983b092f5a2ab471f08a8b734cecfb0df77644
Jeff Peterson <jeff@petersonj.com>
2012-03-18 20:29:31 -0400

    adds support for rdio oauth

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120318201304) do
+ActiveRecord::Schema.define(:version => 20120319000305) do
 
   create_table "feed_entries", :force => true do |t|
     t.string   "guid"
@@ -31,6 +31,9 @@ ActiveRecord::Schema.define(:version => 20120318201304) do
     t.string   "password_digest"
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
   end
 
 end
